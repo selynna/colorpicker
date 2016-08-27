11f55d6a68756729fa0c755f0fc7c77cb4d95125
Selynna Sun <ssun098@gmail.com>
2016-08-27 23:58:07 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 $("input").change(function(readImage) {
   for (var i = 0; i < readImage.originalEvent.srcElement.files.length; i++) {
     var file = readImage.originalEvent.srcElement.files[i];
-    var img = document.createElement("img");
+    var uploadedPic = document.createElement("img");
     var reader = new FileReader();
     reader.onloadend = function() {
-      img.src = reader.result;
-      img.width = 250;
-      img.style = "margin:auto;display:block";
+      uploadedPic.src = reader.result;
+      uploadedPic.width = 250;
+      uploadedPic.style = "margin:auto;display:block";
     }
     reader.readAsDataURL(file);
-    $("input").after(img);
-  }
-});
+    $("input").after(uploadedPic);
+    $("input").attr("id","new-pic");
+    console.log("testing!");
 
+function getHex() {
+  var img = document.getElementById('new-pic');
+  // img.setAttribute('src', 'examples/octocat.png')
+
+  img.addEventListener('load', function() {
+    var vibrant = new Vibrant(img);
+    var swatches = vibrant.swatches();
+    for (var swatch in swatches) {
+        if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
+            console.log(swatch, swatches[swatch].getHex());
+        }
+    }
+});
+}
+  }
+
+
+});
